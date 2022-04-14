b8651c614e719eb0a807162da1a325e4ae3d4340
Janghyeon jung <amapow25@gmail.com>
2022-04-14 16:47:02 +0900

    @RequestParam HTTP 요청으로부터 parameter를 전달 받는 어노테이션
    기본형
    -> @RequestParam("{parameter}") String var
    
    전달 받는 매개변수와 대입하는 변수의 이름이 같으면 매개변수명 생략 가능
    -> @RequestParam String parameter
    
    매개변수명과 대입하는 변수의 이름이 같으면 @RequestParam 자체를 생략할 수 있으나 추천하지 않음
    
    required=true/false
    필수로 넘겨야할 매개변수인지 정할 수 있음
    
    defaultValue = ~
    매개변수가 null 또는 ""이 넘어오는 경우 default 값으로 받을 수 있음
    
    Map<String, Object> paramMap
    모든 매개변수를 Map으로 받아올 수 있음 이때 key는 String임

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,89 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request,
+                               HttpServletResponse response,
+                               HttpMethod method) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}, method={}", username, age, method);
+
+        response.getWriter().write("ok ; " + username + age + "  " + method);
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge,
+            HttpMethod method) {
+        log.info("username={}, age={}, method={}", memberName, memberAge, method);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age,
+            HttpMethod method) {
+        log.info("username={}, age={}, method={}", username, age, method);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam이 없어도 변수를 받아올 수 있으나 명확히 이해하기 힘들어 넣는 것을 추천
+     */
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age, HttpMethod method, HttpServletRequest request) {
+        log.info("username={}, age={}, method={}, URIinfo={}", username, age, method, request.getRequestURI());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam String username,
+            @RequestParam(required = false) Integer age, HttpMethod method, HttpServletRequest request) {       //int는 자바의 기본타입으로 null을 넣을 수 없음, 따라서 null을 핸들링
+                                                                                                                //해야하는 경우 객체인 Integer를 사용하자
+        log.info("username={}, age={}, method={}, URIinfo={}", username, age, method, request.getRequestURI());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(defaultValue = "guest") String username, //defaultvalue는 빈문자도 처리해줌
+            @RequestParam(required = false, defaultValue = "-1") int age, HttpMethod method, HttpServletRequest request) {
+        log.info("username={}, age={}, method={}, URIinfo={}", username, age, method, request.getRequestURI());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParammap(
+            @RequestParam Map<String, Object> paramMap, HttpMethod method, HttpServletRequest request) {
+        log.info("username={}, age={}, method={}, URIinfo={}", paramMap.get("username"), paramMap.get("age"),
+                paramMap, request.getRequestURI());
+        return "ok";
+    }
+
+}
